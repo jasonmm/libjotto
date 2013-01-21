060eb77d4a93b82a6307714759743431e6f64088
Jason <jason@mizher.com>
2013-01-20 20:09:16 -0600

    Initial commit

---
+++ libjotto.go
@@ -0,0 +1,18 @@
+package libjotto
+
+
+import (
+	"container/list"
+)
+
+func GuessResult(guess string, secretWord string) (charsCorrect int, charsInRightPlace int) {
+	for i := 0; i < len(guess); i++ {
+	}
+	return 0, 0
+}
+
+func NextGuess(pastGuesses *list.List) string {
+	return ""
+}
+
+
