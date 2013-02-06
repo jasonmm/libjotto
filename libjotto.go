275a30e858f07e0107d7eefef8e2297437cf344e
Jason <jason@mizher.com>
2013-02-06 13:54:33 -0600

    Modified to return only the number of correct letters.  Added function comments.

--- libjotto.go
+++ libjotto.go
@@ -3,14 +3,25 @@ package libjotto
 
 import (
 	"container/list"
+	"strings"
 )
 
-func GuessResult(guess string, secretWord string) (charsCorrect int, charsInRightPlace int) {
+// Compares the guess to the given secret word and returns the number of 
+// letters the guess got correct and the number of letters the guess put
+// in the correct place.
+func GuessResult(guess string, secretWord string) int {
+	var cnt int = 0
+
 	for i := 0; i < len(guess); i++ {
+		if strings.Contains(secretWord, guess[i:i+1]) {
+			cnt++
+		}
 	}
-	return 0, 0
+	return cnt
 }
 
+// Determines what the next guess should be based on past guesses and their
+// results.
 func NextGuess(pastGuesses *list.List) string {
 	return ""
 }
